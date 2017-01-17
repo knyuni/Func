2f5012771ab456b0757268e303efe18950b890b8
knyuni <ndugirekanyoni@gmail.com>
2017-01-17 14:27:31 +0300

    commit1

---
+++ test.py
@@ -0,0 +1,18 @@
+""" calculate number"""
+
+import unittest
+
+from calc import add_numbers  
+
+class TestAddNumbers(unittest.TestCase):
+	#@unittest.skip("WIP")
+
+	def test_input_is_number(self):
+		self.assertRaises(TypeError, str)
+			
+
+	def test_works_correctly(self):
+		self.assertEquals(add_numbers(3),6)
+
+if __name__ == '__main__':
+    unittest.main()
