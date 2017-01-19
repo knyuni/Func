f67e3af0f8daa7e65354d8bda5e8375fbd6a484f
knyuni <ndugirekanyoni@gmail.com>
2017-01-19 16:08:59 +0300

    Edit Prime_Numbers

---
+++ Fizz_buzz.py
@@ -0,0 +1,17 @@
+def fizz_buzz(number):
+
+	if number%3==0:
+
+	   return "Fizz"
+
+
+  	elif number%5==0:
+  		return "Buzz"
+
+  	elif number%15 == 0: 
+  		return "Fizz Buzz"
+
+  
+
+ 	 else:
+ 	 	return number
